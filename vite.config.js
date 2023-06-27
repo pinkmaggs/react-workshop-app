b2d64f76e405328f37846299381bc6019693e021
Constantine Elias <constantine.elias@hotmail.com>
2023-06-27 15:14:47 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-
-export default {
+export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    assetsDir: '',
-    rollupOptions: {
-      input: 'src/main.jsx',
-    },
-  },
-};
+})
