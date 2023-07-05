1d1b1568ed31c3a550f6f85adb9126da50a1e3fb
Constantine Elias <constantine.elias@hotmail.com>
2023-07-05 15:44:16 +0300

    ssl

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim');
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), basicSsl()],
+  server: {
+    port: 5000,
+    https: true
+  }
 })
